72379adad7f4d373fde833859bec045c7ba341f7
kamkow1 <ikamileq@gmail.com>
2023-06-13 18:28:32 +0200

    Move api controllers to regular controllers

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -2,13 +2,42 @@
 
 namespace App\Controller;
 
-use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\{Response, Request};
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\AccountRepository;
 
 class LoginController extends AbstractController
 {
 	public function index(): Response
 	{
 		return $this->render('login.html.twig');
+	}
+	
+	public function auth(
+			Request $req,
+			AccountRepository $ar
+	): Response
+	{	
+		$json = json_decode($req->getContent(), true);
+
+		$email = $json['email'];
+		$password = $json['password'];
+
+		$user = $ar->findOneByEmail($email);
+		if (!$user) 
+		{
+			return $this->json($status = 404);
+		}
+
+		if (password_verify($password, $user->getPassword()) == 0)
+		{
+			$status = 'Błędne hasło';
+			$data = array('status' => $status);
+			return $this->json($data);
+		}
+
+		$data = array('email' => $email);
+		
+		return $this->json($data, $status = 200);
 	}
 }
--- src/Controller/RegisterController.php
+++ src/Controller/RegisterController.php
@@ -2,13 +2,45 @@
 
 namespace App\Controller;
 
-use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\{Response, Request};
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\AccountRepository;
+use App\Entity\Account;
 
 class RegisterController extends AbstractController
 {
 	public function index(): Response
 	{
 		return $this->render('register.html.twig');
+	}
+	
+	public function create(
+			Request $req,
+			AccountRepository $ar
+	): Response
+	{	
+		$json = json_decode($req->getContent(), true);
+
+		$email = $json['email'];
+		$password = $json['password'];
+		$phone = $json['phone'];
+
+		$user = $ar->findOneByEmail($email);
+		if ($user) 
+		{
+			return $this->json($status = 409);
+		}
+		else
+		{
+			$password = password_hash($password, PASSWORD_BCRYPT);
+
+			$a = new Account();
+			$a->setEmail($email);
+			$a->setPassword($password);
+			$a->setPhone($phone);
+			$ar->save($a, true);
+		}
+
+		return $this->json($status = 201);
 	}
 }
